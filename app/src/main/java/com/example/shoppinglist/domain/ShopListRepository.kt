970deb05d710c25ccdaaa731d4fd07272976f74a
suxa56 <suxa5637@mail.ru>
2022-01-26 13:32:47 +0500

    Create domain

---
+++ app/src/main/java/com/example/shoppinglist/domain/ShopListRepository.kt
@@ -0,0 +1,14 @@
+package com.example.shoppinglist.domain
+
+interface ShopListRepository {
+
+    fun addShopItem(shopItem: ShopItem)
+
+    fun getShopList():List<ShopItem>
+
+    fun getShopItem(shopItemId: Int):ShopItem
+
+    fun editShopItem(shopItem: ShopItem)
+
+    fun deleteShopItem(shopItem: ShopItem)
+}

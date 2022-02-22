4f0b077af456275a1bd169c6f059b8721351b63f
suxa56 <suxa5637@mail.ru>
2022-02-22 13:25:07 +0500

    Setup adapter

--- app/src/main/java/com/example/shoppinglist/presentation/MainActivity.kt
+++ app/src/main/java/com/example/shoppinglist/presentation/MainActivity.kt
@@ -6,43 +6,24 @@ import android.view.LayoutInflater
 import android.widget.LinearLayout
 import android.widget.TextView
 import androidx.lifecycle.ViewModelProvider
+import androidx.recyclerview.widget.RecyclerView
 import com.example.shoppinglist.R
 import com.example.shoppinglist.domain.ShopItem
 
 class MainActivity : AppCompatActivity() {
 
     private lateinit var viewModel: MainViewModel
-    private lateinit var linearLayoutShopList: LinearLayout
+    private lateinit var adapter: ShopListAdapter
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        linearLayoutShopList = findViewById(R.id.linear_layout_shop_view)
+        adapter = ShopListAdapter()
+        val rvShopList = findViewById<RecyclerView>(R.id.rv_shop_list)
+        rvShopList.adapter = adapter
         viewModel = ViewModelProvider(this).get(MainViewModel::class.java)
         viewModel.shopList.observe(this) {
-            showList(it)
-        }
-    }
-
-    private fun showList(list: List<ShopItem>) {
-        linearLayoutShopList.removeAllViews()
-        for (shopItem in list) {
-            val layoutId = if (shopItem.enabled) {
-                R.layout.shop_item_enabled
-            } else {
-                R.layout.shop_item_disabled
-            }
-
-            val view = LayoutInflater.from(this).inflate(layoutId, linearLayoutShopList, false)
-            val cardTitle = view.findViewById<TextView>(R.id.card_title)
-            val cardCount = view.findViewById<TextView>(R.id.card_count)
-            cardTitle.text = shopItem.name
-            cardCount.text = shopItem.count.toString()
-            view.setOnLongClickListener {
-                viewModel.changeEnableState(shopItem)
-                true
-            }
-            linearLayoutShopList.addView(view)
+            adapter.shopList = it
         }
     }
 }
---
+++ app/src/main/java/com/example/shoppinglist/presentation/ShopListAdapter.kt
@@ -0,0 +1,41 @@
+package com.example.shoppinglist.presentation
+
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.TextView
+import androidx.recyclerview.widget.RecyclerView
+import com.example.shoppinglist.R
+import com.example.shoppinglist.domain.ShopItem
+
+class ShopListAdapter: RecyclerView.Adapter<ShopListAdapter.ShopItemViewHolder>() {
+
+    var shopList = listOf<ShopItem>()
+        set(value) {
+            field = value
+            notifyDataSetChanged()
+        }
+
+    class ShopItemViewHolder(view: View) : RecyclerView.ViewHolder(view) {
+        val cardTitle = view.findViewById<TextView>(R.id.card_title)
+        val cardCount = view.findViewById<TextView>(R.id.card_count)
+    }
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ShopItemViewHolder {
+        val view = LayoutInflater.from(parent.context).inflate(R.layout.shop_item_enabled, parent, false)
+        return ShopItemViewHolder(view)
+    }
+
+    override fun onBindViewHolder(holder: ShopItemViewHolder, position: Int) {
+        val shopItem = shopList[position]
+        holder.cardTitle.text = shopItem.name
+        holder.cardCount.text = shopItem.count.toString()
+//        holder.itemView.setOnLongClickListener{
+//            true
+//        }
+    }
+
+    override fun getItemCount(): Int {
+        return shopList.size
+    }
+}
